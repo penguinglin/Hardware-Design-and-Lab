825a7c0ca0dc7755b3e9aea7465571ed0e984fcd
111062118 <jiangdumpling@gmail.com>
2025-09-24 03:23:21 +0800

    first commit

---
+++ LabFile/Lab3/Advance/lab3_advanced/lab3_advanced.v
@@ -0,0 +1,17 @@
+module lab3_basic (
+    input wire clk,
+    input wire rst,
+    input wire mode,
+    input wire play, 
+    input wire right,
+    input wire left,
+    input wire forward,
+    output reg [15:0] LED,
+    output reg [3:0] DIGIT,
+    output reg [6:0] DISPLAY
+);
+    /* Note that output port can be either reg or wire.
+    * It depends on how you design your module. */
+    
+    // add your design here
+endmodule
